102bbcae514f28de906384dccfad1bca5fe9be13
Rajat Manga <Rajatmanga1992@gmail.com>
2023-05-31 10:20:06 +0530

    Delet duplicate Program

---
+++ TestDemo_1/src/DemoGITTest/DeletDup.java
@@ -0,0 +1,27 @@
+package DemoGITTest;
+
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DeletDup {
+
+	public static void main(String[] args) {  
+		  
+        //Initialize array  
+        int [] arr = new int [] {25, 11, 7, 75, 56};  
+        //Initialize min with first element of array.  
+        int min = arr[0];  
+        //Loop through the array  
+        for (int i = 0; i < arr.length; i++) {  
+            //Compare elements of array with min  
+           if(arr[i] <min)  
+               min = arr[i];  
+        }  
+        System.out.println("Smallest element present in given array: " + min);  
+    }  
+}  
