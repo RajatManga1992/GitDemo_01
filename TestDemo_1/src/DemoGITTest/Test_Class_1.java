4c782322165a66613868169a84672be97dd0ecf8
Rajat Manga <Rajatmanga1992@gmail.com>
2023-05-31 10:21:25 +0530

    test class update

--- TestDemo_1/src/DemoGITTest/Test_Class_1.java
+++ TestDemo_1/src/DemoGITTest/Test_Class_1.java
@@ -1,5 +1,34 @@
 package DemoGITTest;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test_Class_1 {
 
+	public static void main(String[] args) {
+		String s = "RajatManga";
+		System.out.println(printduplicateCharacter(s));
+		
+	}
+	
+	public static String printduplicateCharacter(String str) {
+		String newStr= "";
+		 for (int i=0; i<str.length(); i++) {
+			 char ch = str.charAt(i);
+			 if(newStr.indexOf(ch)== -1) {
+				 
+				 newStr = newStr+ch;
+			 }
+		 }
+		 return newStr;
+	}
+	
+	public static void  removeDuplicate_Element_Array(Array a) {
+		
+		
+		
+	}
 }
